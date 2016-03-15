0f3ab5f585fc83805fef0daf40d967adc489b0e4
star <star@star-VirtualBox>
2016-03-16 02:46:41 +0800

    finished

--- hw2.js
+++ hw2.js
@@ -1,9 +1,34 @@
+var status=0;
+
+function change(){
+  if(status==1){
+    document.getElementById("button1").innerHTML="start";
+    status=0;
+  }else{
+    document.getElementById("button1").innerHTML="stop";
+    status=1;
+    minus();
+  }
+}
+function reset(){
+  document.getElementById("num").innerHTML = "00";
+}
 function minus() {
-  if (document.getElementById("num")) {
-    var curr = parseInt(document.getElementById("num").innerHTML);
+  //暫停
+  if(status==0){
+    return 0;
   }
 
-  if (curr && curr <= 60) {
+  if(document.getElementById("time").value==0){
+    var n = 60;
+  }else{
+    var n = document.getElementById("time").value;
+  }
+
+  if(document.getElementById("num")){
+  var curr = parseInt(document.getElementById("num").innerHTML);
+  }
+  if (curr && curr <= n) {
 
     if (curr <= 10) {
       document.getElementById("num").innerHTML = "0" + (curr - 1);
@@ -11,10 +36,10 @@ function minus() {
       document.getElementById("num").innerHTML = curr - 1;
     }
   } else {
-    document.getElementById("num").innerHTML = "60";
+    document.getElementById("num").innerHTML = n;
   }
 
-  setTimeout(function() {
+   setTimeout(function() {
     minus();
   }, 1000);
 };
